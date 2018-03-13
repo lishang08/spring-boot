d34f25236e10f590082daa944f3b34844d8b8cbd
lishang08 <1129958515@qq.com>
2018-03-13 23:16:03 +0800

    first commit

---
+++ gs-spring-boot-demo1/src/main/java/sam/springboot/demo/controller/HelloController.java
@@ -0,0 +1,13 @@
+package sam.springboot.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	@RequestMapping("/")
+	public String home() {
+		return "Hello sam!!";
+	}
+}
